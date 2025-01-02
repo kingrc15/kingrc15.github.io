582f1be1c765589a270f223ad0e58d835f0a3cf1
Ryan King <kingryan14@gmail.com>
2025-01-02 16:37:13 -0600

    updated blog

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "New Student Resources",
-        "excerpt":"Howdy! This page was created to help you get setup with the resources we have here in our lab. If you have any other information you would like to know, please send me a message and let me know. Server Access Connect to the Server Environment Setup Install Jupyter Lab...","categories": ["Setup"],
+        "excerpt":"This page was created to help you get setup with the resources we have here in our lab. If you have any other information you would like to know, please send me a message and let me know. Server Access Connect to the Server Environment Setup Install Jupyter Lab Server...","categories": ["Setup"],
         "tags": ["Setup","Undergraduates","New Students"],
         "url": "/setup/new-student-setup/",
         "teaser": null
@@ -15,5 +15,11 @@ var store = [{
         "excerpt":"        ","categories": [],
         "tags": [],
         "url": "/computational-graphs/",
+        "teaser": null
+      },{
+        "title": "Unimodal Contrastive Pretraining Of Clinical Timeseries",
+        "excerpt":"EHR Modeling with Contrastive Learning: A New Approach to Pretraining Clinical Time Series Data By Ryan King, Shivesh Kodali, Conrad Krueger, Tianbao Yang, and Bobak J. Mortazavi Published in Machine Learning for Health In the fast-evolving world of machine learning, the challenge of harnessing Electronic Health Record (EHR) data remains...","categories": [],
+        "tags": [],
+        "url": "/unimodal-contrastive-pretraining-of-clinical-timeseries/",
         "teaser": null
       }]
